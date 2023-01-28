cac0fa15d06adb7caefa6b5235aa43ee2a3edd2b
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-28 14:07:00 +0300

    custom template

--- main/urls.py
+++ main/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
